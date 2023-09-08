40be520a786a9cbfe35bd1e32382a758cbe5b409
刘洋洋（感知定位） <liuyy@seer-group.com>
2023-09-08 15:08:12 +0800

    2.0 改进 准备移除pangolin

---
+++ mygl.h
@@ -0,0 +1,124 @@
+//
+// Created by moi on 23-9-8.
+//
+
+#ifndef LISTENER_MYGL_H
+#define LISTENER_MYGL_H
+#include <GL/gl.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <GL/glu.h>
+#include "Eigen/Eigen"
+#include "sophus/se3.hpp"
+
+
+
+class Scene {
+ public:
+  Scene(int width = 800, int height = 600, const char* title = "3D Scene Example")
+      : m_width(width), m_height(height), m_title(title) {}
+  
+  ~Scene() {}
+  inline void draw_line(const float x1, const float y1, const float z1,
+                        const float x2, const float y2, const float z2) const {
+    glVertex3f(x1, y1, z1);
+    glVertex3f(x2, y2, z2);
+  }
+  void drawHorizontalGrid();
+ void run();
+
+ private:
+  // 窗口的宽高和标题
+  int m_width;
+  int m_height;
+  const char* m_title;
+  
+  //
+  double lastScrollValue;
+  
+  
+  static bool firstMouse;
+  static float yaw;
+  static float pitch;
+  static float lastX;
+  static float lastY;
+  ///
+  void updateCameraView();
+  void setCameraView(const Sophus::SE3f& cameraPose);
+  // 相机参数
+  static Sophus::SE3f m_cameraPose;
+  
+  // 键盘鼠标事件
+  static bool m_isKeyPressed[4];
+  static float mKeyscroll;
+
+  
+  // 渲染函数
+  void render();
+  
+  // 键盘事件回调函数
+  static void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    std::cout << "d" << std::endl;
+    if (key == GLFW_KEY_W)
+      m_isKeyPressed[0] = (action == GLFW_PRESS || action == GLFW_REPEAT);
+    else if (key == GLFW_KEY_S)
+      m_isKeyPressed[1] = (action == GLFW_PRESS || action == GLFW_REPEAT);
+    else if (key == GLFW_KEY_A)
+      m_isKeyPressed[2] = (action == GLFW_PRESS || action == GLFW_REPEAT);
+    else if (key == GLFW_KEY_D)
+      m_isKeyPressed[3] = (action == GLFW_PRESS || action == GLFW_REPEAT);
+    }
+    
+  static void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+  {
+    // float xpos = static_cast<float>(xposIn);
+    // float ypos = static_cast<float>(yposIn);
+    //
+    // if (firstMouse)
+    // {
+    //   lastX = xpos;
+    //   lastY = ypos;
+    //   firstMouse = false;
+    // }
+    //
+    // float xoffset = xpos - lastX;
+    // float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+    // lastX = xpos;
+    // lastY = ypos;
+    //
+    // float sensitivity = 0.1f; // change this value to your liking
+    // xoffset *= sensitivity;
+    // yoffset *= sensitivity;
+    //
+    // yaw += xoffset;
+    // pitch += yoffset;
+    //
+    // // make sure that when pitch is out of bounds, screen doesn't get flipped
+    // if (pitch > 89.0f)
+    //   pitch = 89.0f;
+    // if (pitch < -89.0f)
+    //   pitch = -89.0f;
+  }
+  
+  static void scrollCallback(GLFWwindow* window, double xoffset, double yoffset)
+  {
+    if (yoffset > 0)
+      mKeyscroll += 1;
+    else
+      mKeyscroll -= 1;
+  }
+  
+  
+  static void mouseScrollEndCallback(GLFWwindow* window, int button, int action, int mods)
+  {
+    
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_REPEAT)
+    {
+      std::cout <<"test" <<std::endl;
+    }
+  }
+  
+  
+};
+
+#endif  //LISTENER_MYGL_H
--- vis.cpp
+++ vis.cpp
@@ -4,8 +4,8 @@
 
 VisTool::VisTool() {
 	m_window_name2 ="plot";
-	pangolin::CreateWindowAndBind(m_window_name, 1920, 1080);
-	pangolin::CreateWindowAndBind(m_window_name2, 1920, 1080);
+	pangolin::CreateWindowAndBind(m_window_name, 1024, 768);
+	pangolin::CreateWindowAndBind(m_window_name2, 1024, 768);
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -19,10 +19,12 @@ VisTool::~VisTool() {
 
 void VisTool::run() {
 	
+ 
+ 
 	pangolin::BindToContext(m_window_name);
 	pangolin::OpenGlRenderState s_cam(
-			pangolin::ProjectionMatrix(1024, 768, 500, 500, 512, 389, 0.1, 10000),
-			pangolin::ModelViewLookAt(-2, 0, -2, 0, 0, 0, pangolin::AxisZ));
+			pangolin::ProjectionMatrix(1024, 768, 500, 500, 512, 389, 0.1, 1000),
+			pangolin::ModelViewLookAt(-10, 0, 10, 0, 0, 0, pangolin::AxisZ));
 	pangolin::Handler3D handler(s_cam);
 	pangolin::View &d_cam = pangolin::CreateDisplay()
 			.SetBounds(0.0, 1.0, 0.0, 1.0, -1024.0f / 768.0f)
