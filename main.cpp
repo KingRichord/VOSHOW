55c953b1739920d3fb782534ac8fa2cbfea0a443
刘洋洋（感知定位） <liuyy@seer-group.com>
2023-09-12 17:53:13 +0800

    可视化2.0优化版本

--- main.cpp
+++ main.cpp
@@ -4,31 +4,28 @@
 #include "dispatch.h"
 #include "points.h"
 #include "sophus/se3.hpp"
-#include "mygl.h"
 int main(int argc, char** argv) {
-  Scene scene;
-  scene.run();
-        // Points<Eigen::Vector3f> a;
-	// Dispatch dp;
-	// zmq::context_t context(1);
-	// zmq::socket_t subscriber(context, ZMQ_SUB);
-	// std::string TOPIC = "";
-	// subscriber.setsockopt(ZMQ_SUBSCRIBE, TOPIC.c_str(), TOPIC.length());
-	// subscriber.connect("tcp://127.0.0.1:5555");
-	// // subscriber.connect("tcp://192.168.192.5:8002");
-	// while (subscriber.connected())
-	// {
-	// 	zmq::message_t message;
-        //         zmq::recv_result_t res;
-	// 	res = subscriber.recv(message,zmq::recv_flags::none);
-	// 	if (res.has_value())
-	// 	{
-	// 		auto reply_str = std::string(static_cast<char *>(message.data()), message.size());
-	// 		auto json_data = nlohmann::json::parse(reply_str);
-	// 		dp.MsgDispatch(json_data);
-	// 	}
-	// }
-	// subscriber.close();
-	// context.close();
-	return 0;
+      Points<Eigen::Vector3f> a;
+      Dispatch dp;
+      zmq::context_t context(1);
+      zmq::socket_t subscriber(context, ZMQ_SUB);
+      std::string TOPIC = "";
+      subscriber.setsockopt(ZMQ_SUBSCRIBE, TOPIC.c_str(), TOPIC.length());
+      subscriber.connect("tcp://127.0.0.1:5555");
+      // subscriber.connect("tcp://192.168.192.5:8002");
+      while (subscriber.connected())
+      {
+              zmq::message_t message;
+              zmq::recv_result_t res;
+              res = subscriber.recv(message,zmq::recv_flags::none);
+              if (res.has_value())
+              {
+                      auto reply_str = std::string(static_cast<char *>(message.data()), message.size());
+                      auto json_data = nlohmann::json::parse(reply_str);
+                      dp.MsgDispatch(json_data);
+              }
+      }
+      subscriber.close();
+      context.close();
+      return 0;
 }
